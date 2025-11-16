f072adc3df5a8b2f2f419c090f2e21c09e0caa4b
jmvillanueva-dev <jhonny.villanueva@epn.edu.ec>
2025-11-16 00:58:22 -0500

    feat: add index file for use cases related to user routines, workout logs, and progress photos

---
+++ src/domain/useCases/index.ts
@@ -0,0 +1,16 @@
+export * from "./AuthUseCase";
+export * from "./ExerciseUseCase";
+export * from "./MediaUseCase";
+export * from "./ProfileUseCase";
+export * from "./RoutineUseCase";
+export * from "./TrainingPlanUseCase";
+
+// Interfaces de casos de uso para usuario
+export * from "./IProgressPhotoUseCase";
+export * from "./IUserRoutineUseCase";
+export * from "./IWorkoutLogUseCase";
+
+// Casos de uso concretos para usuario
+export * from "./ProgressPhotoUseCase";
+export * from "./UserRoutineUseCase";
+export * from "./WorkoutLogUseCase";
